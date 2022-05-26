1a3a50eca5c2f324d88718ed5a7f1a3d740d2e54
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2022-05-26 16:33:32 +0100

    Created news folder

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -16,6 +19,24 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	tpl.Execute(w, nil)
 
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
 }
 
 func main() {
@@ -29,8 +50,14 @@ func main() {
 		port = "3000"
 	}
 
+	fs := http.FileServer(http.Dir("assets"))
+
 	mux := http.NewServeMux()
 
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler)
+
 	http.ListenAndServe(":"+port, mux)
 }
